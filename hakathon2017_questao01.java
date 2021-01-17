64b25ca9005b96ae0e976c30efb22762f67b293f
Ramonbr14 <ramonbr14@gmail.com>
2021-01-17 17:12:54 -0400

    Consegui fazer o codigo separa os numeros numa array list, mas não esta adicionando o ultimo numeo

--- hakathon2017_questao01.java
+++ hakathon2017_questao01.java
@@ -6,19 +6,36 @@ public class Main
     
     public static void ehomaior(String entrada){
 	
-	int tam = entrada.length();
-	List<String> v1;
-	String v2 = "";
-	//List<String> numeros = ["0","1","2","3","4","5","6","7","8","9"]
-    for(int i = 0; i< tam; i++){
-		System.out.println(" DENTRO DO FOR "+ i);
-		
-		v2 = v2+entrada[i];	
-		if(entrada[i] ==" "){
-				v1.add(v2);
-		}
-	System.out.println("ENTROUUUUUUU!!!!!!!! "+ tam);
-    }
+	
+	List<String> v1 = new ArrayList<>();
+	String v2="";
+	int y = 0;
+	int i;
+	for(i=0;i<entrada.length();i++){
+	    if(entrada.charAt(i) == ' '){
+	        
+	        v1.add(v2);
+	        v2 = "";
+	        System.out.println("VOLTA "+i+ " ACHOU O ESPAÇO e o valor de v1 na posicao "+y+" é "+v1.get(y));
+	        y++;
+	        }else
+	            {
+	            if((entrada.charAt(i) >= '0') && (entrada.charAt(i) <= '9')){
+	                v2 = v2+String.valueOf(entrada.charAt(i));
+	                System.out.println("VOLTA "+i+ "valor de v2: "+v2   );
+	                }    
+	            }
+	 
+
+	}
+	int n = v1.size();
+	for (i=0; i<n; i++) {
+      System.out.printf("Posição %d- %s\n", i, v1.get(i));
+    }   
+	 
+	//O ULTIMO NUMERO NÃO TA ENTRANDO DESCOBRIR O PORQUER
+	
+
 }
 
 
