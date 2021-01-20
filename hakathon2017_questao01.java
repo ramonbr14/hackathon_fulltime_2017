31ac1d526efeb1dd9b45d64486833b52a661bff0
Ramonbr14 <ramonbr14@gmail.com>
2021-01-20 17:45:02 -0400

    Consegui fazer o codigo separa os numeros numa array list, mas não esta adicionando o ultimo numeo

--- hakathon2017_questao01.java
+++ hakathon2017_questao01.java
@@ -4,38 +4,44 @@ import java.util.Scanner;  // Import the Scanner class
 public class Main
 {
     
-    public static void ehomaior(String entrada){
+    public static void separaNumero(String entrada){
 	
-	
+	    
 	List<String> v1 = new ArrayList<>();
 	String v2="";
 	int y = 0;
 	int i;
+	int n = entrada.length();
+	System.out.printf("VALOR DE n é: %d \n", n);
+	//System.out.printf("o tamanho da entrada.length é: %d \n", n);
 	for(i=0;i<entrada.length();i++){
-	    if(entrada.charAt(i) == ' '){
+	    //System.out.printf("valor de y %d \n", y);
+	    //System.out.println("VOLTA "+i+ "valor de v1: ");
+	        System.out.printf("VALOR DE i é: %d \n", i);
 	        
+	        
+	    if(entrada.charAt(i) == ' '|| i == n){
+	        System.out.printf("o v2 QUE VAI ENTRAR EM v1: %s \n", v2);
 	        v1.add(v2);
 	        v2 = "";
-	        System.out.println("VOLTA "+i+ " ACHOU O ESPAÇO e o valor de v1 na posicao "+y+" é "+v1.get(y));
+	        System.out.printf("valor de v1 posicao %d = %s\n", y, v1.get(y));
+	        //System.out.println("VOLTA "+i+ " ACHOU O ESPAÇO e o valor de v1 na posicao "+y+" é "+v1.get(y));
 	        y++;
-	        }else
-	            {
-	            if((entrada.charAt(i) >= '0') && (entrada.charAt(i) <= '9')){
-	                v2 = v2+String.valueOf(entrada.charAt(i));
-	                System.out.println("VOLTA "+i+ "valor de v2: "+v2   );
+	    }else
+	       {
+	        if((entrada.charAt(i) >= '0') && (entrada.charAt(i) <= '9')){
+	           v2 = v2+String.valueOf(entrada.charAt(i));
+	           System.out.println("\n VOLTA do for " + i + " valor de v2: "+v2 +"\n"   );
 	                }    
 	            }
-	 
-
 	}
-	int n = v1.size();
+	v1.add(v2); // o ultimo numero só entra nessa instrucao
+	n = v1.size();
+	System.out.printf("TAMANHO %d \n", n);
 	for (i=0; i<n; i++) {
-      System.out.printf("Posição %d- %s\n", i, v1.get(i));
+      System.out.printf("Posição %d = %s\n", i, v1.get(i));
     }   
-	 
-	//O ULTIMO NUMERO NÃO TA ENTRANDO DESCOBRIR O PORQUER
-	
-
+    System.out.println("FIM DA INSTRUCAO \n");
 }
 
 
@@ -44,7 +50,7 @@ public class Main
 		Scanner s = new Scanner(System.in);
 		System.out.println("ESCREVA 3 NUMEROS NA MESMA LINHA: \n");
 		String entrada = s.nextLine();
-		ehomaior(entrada);
+		separaNumero(entrada);
 		
 		
 	}
