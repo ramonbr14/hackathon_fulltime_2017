bd6556337b12cc8a318062fbd753a9732e8286ec
Ramonbr14 <ramonbr14@gmail.com>
2021-02-25 19:13:22 -0400

    Abri o arquivo pra resolver o Problema B

---
+++ hakathon2017_questao02.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;  // Import the Scanner class
+public class Main
+{
+    
+  
+    
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("DISTANCIA ENTRE PONTOS \n ENTRE COM AS COORDENADAS DOS PONTO X \n");
+		String entrada = s.nextLine();
+		System.out.println("DISTANCIA ENTRE PONTOS \n ENTRE COM AS COORDENADAS DOS PONTO Y \n");
+		String entrada = s.nextLine();
+		System.out.println("PROGRAMA ENCERRADO \n");
+		
+	}
+}
