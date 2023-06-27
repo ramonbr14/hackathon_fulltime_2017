eab4af12022f71949df491c3b89d585c82638318
RamonDevBr14 <ramonbr14@gmail.com>
2023-06-27 18:56:49 -0400

    MODIFICADO CON SUCESSO A SEPARACAO DE NUMEROS DA ENTRADA, AVALIAR O POR QUE O RESULTADO ESTA DANDO ZERO NA FUNCAO DISTANCIA

--- hakathon2017_questao02.java
+++ hakathon2017_questao02.java
@@ -1,49 +1,51 @@
 //https://www.online-java.com/
 
 import java.util.ArrayList;
+import java.util.Arrays;  
 import java.util.List;
 import java.util.Scanner;  // Import the Scanner class
 public class Main
 {
-    public static void separaNumero(String entrada){
+    public static List<String> separaNumero(String entrada){
+    List<String> listnumeros = new ArrayList<String>();
 	//Codigo para Extrair os numeros da entrada
-	try {	List<Double> numeros = new ArrayList<Double>();
-	    String v2="";
-	    int i;
-	    int m = entrada.length();
-	
-	    for(i=0;i<entrada.length();i++){
-	        if(entrada.charAt(i) == ' '|| i == m){
-	            numeros.add(Double.parseDouble(v2));
-	            System.out.println("Valor de v2: " + v2);
-	            v2 = "";
+	try {	
+	    
+	    
+	    String a = "";
+	    
+	    for(int i=0;i<entrada.length();i++){
+	       
+	        if(entrada.charAt(i)!= ' '){
+	            a = a+String.valueOf(entrada.charAt(i));
+	            // System.out.println("separaNumero volta:  "+ i);
 	        }else{
-	            if((entrada.charAt(i) >= '0') && (entrada.charAt(i) <= '9'||(entrada.charAt(i) == '.'))){
-	                v2 = v2+String.valueOf(entrada.charAt(i));
-	                System.out.println("Valor de v2: " + v2);
-	                }    
-	            }
+	           // System.out.println("separaNumero valor de a:  "+ a);
+	            listnumeros.add(a);
+	            a = "";
 	    }
-	
-	numeros.forEach(System.out::println);
-    
+	    listnumeros.add(a);
+	   }
+	   
     }catch (Exception e) {
         System.out.printf("ERRO NA EXECUÇÃO, ENTRADA FORA DO LIMITE");
     }
-}    
-    
-    
-    
-    
-    public static void distancia(String x1,String x2,String y1,String y2){
-		
+    System.out.println("Fim do separaNumero");
+    //listnumeros.forEach(System.out::println);
+    return listnumeros;
+    }
+
+
+
+    //public static void distancia(String x1,String x2,String y1,String y2){
+	public static void distancia(List<String> entradaNumeros){	
 		
 		try{
 			
-				Double fxa = Double.parseDouble(x1);
-				Double fxb = Double.parseDouble(x2);
-				Double fya = Double.parseDouble(y1);
-				Double fyb = Double.parseDouble(y2);
+				Double fxa = Double.parseDouble(entradaNumeros.get(0));
+				Double fxb = Double.parseDouble(entradaNumeros.get(1));
+				Double fya = Double.parseDouble(entradaNumeros.get(2));
+				Double fyb = Double.parseDouble(entradaNumeros.get(3));
 				
 				Double dist = Math.pow(fxb-fxa,2);
 				//Float dist = Float.parseFloat(Math.sqrt(Math.pow(fxb-fxa,2)+Math.pow(fyb-fya,2)));
@@ -73,13 +75,15 @@ public class Main
 		*/
 		
 		//Entrada em Duas linhas
-		
+		List<String> entradaNumeros = new ArrayList<String>();
 		Scanner s = new Scanner(System.in);
 		System.out.println("ESCREVA as coordenadas: \n");
 		String entrada = s.nextLine();
-		System.out.println(entrada);
-		separaNumero(entrada);
-		
+		entradaNumeros = separaNumero(entrada);
+		entrada = s.nextLine();
+		entradaNumeros.addAll(separaNumero(entrada));
+		//entradaNumeros.forEach(System.out::println);
+		distancia(entradaNumeros);
 		System.out.println("PROGRAMA ENCERRADO \n");
 	}
 }
